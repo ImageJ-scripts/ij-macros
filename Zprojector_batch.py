f788f663e07ed9c2ad8e22a2aa002fbe908d1f52
Igor Bonacossa Pereira <uqibonac@uq.edu.au>
2019-04-10 17:31:02 +1000

    Modularized Zprojector

---
+++ Zprojector_batch.py
@@ -0,0 +1,23 @@
+# Zprojector_batch.py
+# IBP
+# i.bonacossapereira@uq.edu.au
+
+#@File (label="Images folder", style="directory") img_folder
+#@File (label="Save folder", style="directory") save_folder
+#@String (label="Images extension", value=".tif") ext
+#@String (label="Projection method", choices={"max","min","avg","sum", "sd", "median"}) method
+
+# Bootstrap to extend modules search path #
+from sys import path
+import os.path
+from java.lang.System import getProperty
+jython_scripts = os.path.join(getProperty('user.home'), 'Jython_scripts')
+path.append(jython_scripts)
+#=========================================#
+
+from IBPlib.ij import Projector
+
+savefolder = save_folder.getPath()
+imgfolder = img_folder.getPath()
+projector = Projector(savefolder, imgfolder, ext, method)
+projector.run()
